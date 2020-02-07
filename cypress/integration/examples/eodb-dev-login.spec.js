16afb7cc83527fabcd4b68e4eaec25cbe39b05f9
VPBelo <venus.belo.rhi@gmail.com>
2020-02-07 14:56:18 +0800

    login

--- cypress/integration/examples/eodb-dev-login.spec.js
+++ cypress/integration/examples/eodb-dev-login.spec.js
@@ -1,12 +1,12 @@
 describe('EASE OF DOING BUSINESS', () => {
 
-    it('Navigate to eodb-dev.ml', () => {
-        cy.visit('https://eodb-dev.ml/app')
+    it('Navigate to eodb-qa.ml', () => {
+        cy.visit('https://eodb-qa.ml/app')
     })
     it('Applicant Login', () => {
         cy.get(':nth-child(6) > a > .ant-btn').click({ delay: 100 })
-        cy.get('#login-email').type('antoniobasasjr@gmail.com' , { delay: 100 })
-        cy.get('#login-password').type('1234567890' , { delay: 100 })
+        cy.get('#login-email').type('vbelo@centralizedinc.com', { delay: 100 })
+        cy.get('#login-password').type('Password123', { delay: 100 })
         cy.get('#login-btn').click()
 
     })
