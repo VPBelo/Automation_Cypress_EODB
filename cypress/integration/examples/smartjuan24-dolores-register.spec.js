d5014c949b8b031421e27cbee46d07a7f2323148
VPBelo <venus.belo.rhi@gmail.com>
2020-03-03 17:55:26 +0800

    update

--- cypress/integration/examples/smartjuan24-dolores-register.spec.js
+++ cypress/integration/examples/smartjuan24-dolores-register.spec.js
@@ -5,8 +5,13 @@ describe('smartjuan24-dolores', () => {
             // cy.visit('http://192.168.1.114:8080/app')
     })
     it('register', () => {
-        cy.get(':nth-child(7) > .ant-btn').click()
-        cy.get('.account-form > :nth-child(1) > :nth-child(1) > .ant-row > .ant-form-item-control-wrapper > .ant-form-item-control > .ant-form-item-children > .ant-input').type('Vince', { delay: 100 });
+        // cy.get(':nth-child(7) > a > .ant-btn').click()
+        // cy.get('button[class="anticon anticon-user"]').click().wait(5000)
+        cy.get(':nth-child(7) > a > .ant-btn').click().wait(5000)
+            // cy.get('.account-form > :nth-child(1) > :nth-child(1) > .ant-row > .ant-form-item-control-wrapper > .ant-form-item-control > .ant-form-item-children > .ant-input').type('Vince', { delay: 100 });
+            // cy.get(':nth-child(7) > [data-top="233"]').type('Vince');
+        cy.get('#first-name').type('Vince', { delay: 100 })
+        cy.get('#last-name').type('Belo', { delay: 100 })
         cy.get(':nth-child(1) > :nth-child(2) > .ant-row > .ant-form-item-control-wrapper > .ant-form-item-control > .ant-form-item-children > .ant-input').type('Belo', { delay: 100 });
         cy.get(':nth-child(2) > .ant-form-item-control-wrapper > .ant-form-item-control > .ant-form-item-children > .ant-input').type('vbelo@centralizedinc.com', { delay: 100 });
         cy.get(':nth-child(3) > :nth-child(1) > .ant-row > .ant-form-item-control-wrapper > .ant-form-item-control > .ant-form-item-children > .ant-input').type('admin', { delay: 100 });
