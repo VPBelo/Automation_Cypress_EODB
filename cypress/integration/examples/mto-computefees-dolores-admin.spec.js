55146faf9fdbd3b12857d0b13fdf653d10d6f2ff
VPBelo <VPBelo>
2020-03-11 13:14:58 +0800

    update

---
+++ cypress/integration/examples/mto-computefees-dolores-admin.spec.js
@@ -0,0 +1,36 @@
+describe('EASE OF DOING BUSINESS ADMIN PORTAL', () => {
+
+    it('Navigate to eodb-qa.ml site', () => {
+        cy.visit('https://dolores.smartjuan24.com/admin')
+
+    })
+
+    it('MTO Department Login', () => {
+        cy.get('#department-index2').click()
+
+    })
+
+    it('Login Admin MTO Approver', () => {
+        cy.get('input[placeholder="Email Address"]').type('mto@gmail.com');
+        cy.get('input[placeholder="Enter Password"]').type('admin');
+        cy.get(".ant-btn").click()
+        // cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > :nth-child(1) > .card_btn > .ant-card-body > .ant-row > .ant-col-18 > :nth-child(2)').click()
+        // cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > :nth-child(1) > .card_btn > .ant-card-body > .ant-row > .ant-col-18 > :nth-child(2)').click() // // For Processing
+        // cy.get(':nth-child(3) > :nth-child(2) > :nth-child(1) > :nth-child(1) > .card_btn > .ant-card-body > .ant-row > .ant-col-18 > span').click() // In-Progress
+        // cy.get('.ant-btn-primary').click()
+        // cy.get('.ant-card-head-title > .ant-row > [align="right"] > .ant-btn').click()
+        // cy.get('.ant-form-item-children > .ant-input').type('Complete')
+        // cy.get('.ant-btn-group > .ant-btn-primary').click()
+        // cy.get('.ant-popover-buttons > .ant-btn-primary').click()
+        // cy.get('[aria-selected="false"] > span').click()
+        // cy.get(':nth-child(1) > .ant-collapse-header').click()
+        // cy.get(':nth-child(2) > .ant-collapse-header').click()
+        // cy.scrollTo('top')
+        // cy.get('.ant-checkbox-input').click()
+        // cy.get('.ant-card-body > .ant-input').type('Approved')
+        // cy.get('.ant-row-flex > [align="right"] > .ant-btn').click()
+        // cy.get(':nth-child(8) > :nth-child(1) > .ant-popover > .ant-popover-content > .ant-popover-inner > .ant-popover-inner-content > .ant-popover-buttons > .ant-btn-primary').click()
+        cy.get('.ant-menu > :nth-child(6)').click()
+        cy.get('.ant-btn-primary').click()
+    })
+})
