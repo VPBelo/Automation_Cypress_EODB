b0ab8420fa4f946eadecad0ee8d267a5531416c3
VPBelo <VPBelo>
2020-03-12 14:52:01 +0800

    update

---
+++ cypress/integration/EODB/Login.js
@@ -0,0 +1,28 @@
+describe('SmartJuan24 Login User Account', () => {
+
+    /**
+     * @description initialize fixtures
+     * 
+     */
+    beforeEach(() => {
+        cy.fixture('settings.json').as('settings')
+        cy.fixture('user_account.json').as('user_account')
+      })
+
+
+    it('User must be able to Login', function() {
+        cy.scrollTo('top')
+        cy.get(':nth-child(6) > a > .ant-btn').click({
+            delay: 100
+        })
+        // cy.get('#login-email').type(this.user_account.username)
+        cy.get('#login-email').type(Cypress.env("username"))
+        cy.get('#login-password').type(this.user_account.password)
+        cy.get('#login-btn').click()
+        cy.scrollTo('top')
+
+    })
+
+
+
+})
--- cypress/integration/EODB/Registration.js
+++ cypress/integration/EODB/Registration.js
@@ -1,4 +1,4 @@
-describe('Registration', () => {
+describe('SmartJuan24 Registration', () => {
 
 
     /**
@@ -9,26 +9,23 @@ describe('Registration', () => {
         cy.fixture('settings.json').as('settings')
         cy.fixture('user_account.json').as('user_account')
       })
-    
 
-    it('Navigate to Dolores Smartjuan24 EODB', function() {
-        cy.visit(this.settings.base_url)
-    })
-
-    it('Applicant Register', function() {
+    it('Applicant should be able to sign-up', function() {
+        var username = `${Math.random()}${this.user_account.email}`
         cy.get(':nth-child(7) > a > .ant-btn').click()
         cy.get(':nth-child(7) > a > .ant-btn').click({
             delay: 100
         })
         cy.get('#first-name').type(this.user_account.firstname)
         cy.get('#last-name').type(this.user_account.lastname)
-        // cy.get('#reg-email').type(`${Math.random()}${this.user_account.email}`)
-        cy.get('#reg-email').type(this.user_account.email)
+        cy.get('#reg-email').type(username)
         cy.get('#reg-password').type(this.user_account.password)
         cy.get('#reg-confirm-password').type(this.user_account.password)
         cy.scrollTo('top')
         cy.get("#reg-btn").click()
         cy.scrollTo('top')
+
+        Cypress.env("username",username)
 
     })
 
