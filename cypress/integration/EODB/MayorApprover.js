9e8ffe2798a25bdad313ab258801141338a0349c
VPBelo <VPBelo>
2020-03-13 14:54:56 +0800

    update

--- cypress/integration/EODB/MayorApprover.js
+++ cypress/integration/EODB/MayorApprover.js
@@ -17,7 +17,6 @@ describe('Office of the Mayor', () => {
         cy.get('input[placeholder="Enter Password"]').type(this.mayor_account.password)
         cy.get(".ant-btn").click().wait(5000)
         cy.scrollTo('top')
-        // cy.get('input[placeholder="Search Keyword (eg. Reference Number, Business Name)"]').type('1584004772722200')
         cy.get('input[placeholder="Search Keyword (eg. Reference Number, Business Name)"]').type(Cypress.env("reference"))
         cy.get('.ant-col-18 > :nth-child(2)').click()
         cy.get('.ant-btn-primary').click().wait(5000)
