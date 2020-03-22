5a6c145802b279b003d06559834a747aa3b91426
VPBelo <venus.belo.rhi@gmail.com>
2020-03-23 07:18:19 +0800

    cypress-update

--- cypress/integration/EODB/AssessorRequestCertificate.js
+++ cypress/integration/EODB/AssessorRequestCertificate.js
@@ -25,13 +25,19 @@ describe('User Assessors Request Portal', () => {
             force: true
         })
         cy.scrollTo('top').wait(2000)
-        cy.get('.ant-col-xs-24 > .ant-row-flex-center > :nth-child(3)').click()
-        // cy.get('#my-application').click({ force: true })
-        // cy.scrollTo('top')
-        // cy.get(':nth-child(5) > span > #v-step-5').click({
-        //     force: true
-        // })
-        // cy.get('.ant-modal-confirm-btns > .ant-btn-primary').click().wait(5000)
+        cy.get('.ant-col-xs-24 > .ant-row-flex-center > :nth-child(3)').click().wait(5000)
+        cy.get(':nth-child(1) > .form-label > .ant-form-item-control-wrapper > .ant-form-item-control > .ant-form-item-children > .form-input > .ant-select-selection').click()
+        cy.get('li[style="user-select: none;"]').contains('CERTIFIED TRUE COPY OF TAX DECLARATION').click();
+        cy.get(':nth-child(2) > .form-label > .ant-form-item-control-wrapper > .ant-form-item-control > .ant-form-item-children > .form-input > .ant-select-selection').click()
+        cy.get('li[style="user-select: none;"]').contains('33445412122C').click();
+        cy.get('.ant-btn-group > .ant-btn-primary').click()
+        cy.get('[align="end"] > .ant-btn').click({ force: true })
+        cy.get('#my-application').click({ force: true })
+        cy.scrollTo('top')
+        cy.get(':nth-child(5) > span > #v-step-5').click({
+            force: true
+        })
+        cy.get('.ant-modal-confirm-btns > .ant-btn-primary').click().wait(5000)
     })
 
 
